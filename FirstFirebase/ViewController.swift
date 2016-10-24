3fc42253d59050f34dc4912236fe69c3126df7ab
Sgmedical <sgmedical@sgs-macbook-air.local>
2016-10-24 11:42:45 +0800

    step 2: create buttons to send out events.

--- FirstFirebase/ViewController.swift
+++ FirstFirebase/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
@@ -20,6 +21,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func krakenBtnAction(_ sender: UIButton) {
+        
+        //如果是使用事先定義好的 events (kFIREventLogin, kFIREventSearch..., 則可以用下拉選單)
+        //在下列中，因為使用自定義按鈕,所以，在參數 withName: 直接帶字串即可
+        FIRAnalytics.logEvent(withName: "Press_button", parameters: nil)
+    }
 
+    @IBAction func switchBtnAction(_ sender: UISwitch) {
+        
+        FIRAnalytics.logEvent(withName: "flip_Switch", parameters: nil)
+    }
+    
+    @IBAction func sliderBtnAction(_ sender: UISlider) {
+        //使用 slider button, 必須使用 touch up events
+        //原因是因為 XCode 預設是使用 value changed event, 一旦使用它，就會產生一大堆的 event (因為刻度造成改變)
+        /*FIRAnalytics.logEvent(withName: <#T##String#>, parameters: <#T##[String : NSObject]?#>)
+        */
+        FIRAnalytics.logEvent(withName: "adjust_slider", parameters: ["newValue": sender.value as NSObject])
+        
+    }
+    
 }
 
